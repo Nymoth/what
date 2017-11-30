e2426b566cdc01400aee90f62bd986ee7a522780
Miguel S <miguel.sarries@beeva.com>
2017-11-30 17:37:28 +0100

    ijo

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const express = require('express')
     , app = express()
     , Datastore = require('nedb');
 
-// app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-// app.use('/assets', express.static('node_modules'));
+app.use(bodyParser.json());
 
 const dbs = [
   'characters'
@@ -18,11 +14,21 @@ const dbs = [
 dbs.forEach(dbname => {
   const db = new Datastore({ filename: `./data/${dbname}.json`, autoload: true });
 
-  const url = `/${dbname}/`;
-  const idUrl = `${url}:id`;
+  const url = `/${dbname}`;
+  const idUrl = `${url}/:id`;
     
   app.get(url, (req, res) => {
-    db.find({}).toArray((err, results) => {
+    db.find({}, (err, results) => {
+      assert.equal(null, err);
+      res.json({
+        results
+      });
+    });
+  });
+
+  app.get(idUrl, (req, res) => {
+    const _id = +req.params.id
+    db.findOne({ _id }, (err, results) => {
       assert.equal(null, err);
       res.json({
         results
@@ -32,31 +38,40 @@ dbs.forEach(dbname => {
 
   app.post(url, (req, res) => {
     const data = req.body;
-    db.insertOne(data, (err, r) => {
+
+    db.find().sort({ _id: -1 }).limit(1).exec((err, r) => {
       assert.equal(null, err);
-      res.json(r);
+
+      const lastId = +r[0]._id;
+      data._id = lastId + 1;
+
+      db.insert(data, (err, ir) => {
+        assert.equal(null, err);
+        res.json(ir);
+      });
     });
+
   });
 
   app.put(idUrl, (req, res) => {
-    const _id = new mongodb.ObjectId(req.params.id)
+    const _id = +req.params.id
         , data = req.body;
 
-    db.findOneAndUpdate({ _id }, data, (err, r) => {
+    db.update({ _id }, data, (err, r) => {
       assert.equal(null, err);
       res.json(r);
     });
   });
 
   app.delete(idUrl, (req, res) => {
-    const _id = new mongodb.ObjectId(req.params.id);
-    db.deleteOne({ _id }, (err, r) => {
+    const _id = +req.params.id;
+    db.remove({ _id }, (err, r) => {
       assert.equal(null, err);
       res.json({ _id });
     });
   });
 
-  console.log('DB OK');
+  console.log(`DB ${dbname} OK`);
 });
 
 app.listen(3000);
